0da8660e0c3a1e9f79b387c313b48bf59401d45f
Brook-yohohoho <nosegay.brook@gmail.com>
2024-05-30 10:53:53 +0800

    Delete gas-reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,6 @@ import '@matterlabs/hardhat-zksync-verify';
 import '@matterlabs/hardhat-zksync-upgradable';
 import * as dotenv from 'dotenv';
 import { NetworksUserConfig, NetworkUserConfig } from 'hardhat/src/types/config';
-import 'hardhat-gas-reporter';
 
 import './script/deploy_zkjump';
 import './script/deploy_mock_token';
@@ -21,9 +20,6 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: 'typechain',
     target: 'ethers-v6',
-  },
-  gasReporter: {
-    enabled: true,
   },
   solidity: {
     compilers: [
